08f571ea46a4b075565717d0daed656525085be9
roccogiordano <rocco.giordano573@gmail.com>
2024-10-24 21:08:11 +0200

    feat: functions declaration

--- Snack2/main.js
+++ Snack2/main.js
@@ -41,3 +41,39 @@ const footballTeams = [
 ];
 
 //
+
+
+// getRndInteger Function
+
+/**
+ * Generates a random number between min and max (both included).
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
+
+function getRndInteger(min, max) {
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+
+};
+
+//
+
+
+// generateRandomStats Function
+
+/**
+ * Generates random stats for a given football team.
+ * @param {object} team
+ * @returns {object}
+ */
+
+function generateRandomStats(team) {
+
+    team.points = getRndInteger(0, 114);
+
+    team.fouls = getRndInteger(0, 50);
+
+};
+//
