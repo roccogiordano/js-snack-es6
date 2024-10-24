3f7647b5e70b9cd58b9c0dba969e7d2da12b81a6
roccogiordano <rocco.giordano573@gmail.com>
2024-10-24 20:11:47 +0200

    feat: array declaration

---
+++ Snack1/main.js
@@ -0,0 +1,38 @@
+// bikesList Array Declaration
+
+const bikesList = [
+
+
+    // bike in bikesList
+
+    {
+        name: "Bike1",
+        weight: 7.2
+    },
+
+    {
+        name: "Bike2",
+        weight: 8.4
+    },
+
+    {
+        name: "Bike3",
+        weight: 9.6
+    },
+
+    {
+        name: "Bike4",
+        weight: 7.8
+    },
+
+    {
+        name: "Bike5",
+        weight: 8.5
+    }
+
+    //
+
+
+];
+
+//
