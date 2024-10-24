5b20d68cbe16e9fa0c414255a6106cd359cff102
roccogiordano <rocco.giordano573@gmail.com>
2024-10-24 20:52:28 +0200

    feat: array declaration

---
+++ Snack2/main.js
@@ -0,0 +1,33 @@
+// footballTeams Array Declaration
+
+const footballTeams = [
+
+
+    // team in footballTeams
+
+    {
+        name: "Inter"
+    },
+
+    {
+        name: "Juventus"
+    },
+
+    {
+        name: "Napoli"
+    },
+
+    {
+        name: "Lazio"
+    },
+
+    {
+        name: "Bologna"
+    }
+
+    //
+
+
+];
+
+//
