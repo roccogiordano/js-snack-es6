c2340658e9a283a28780d33f62270cb0e2a4e91c
roccogiordano <rocco.giordano573@gmail.com>
2024-11-21 19:10:19 +0100

    feat: added variables and arrays declaration

---
+++ Snack3/main.js
@@ -0,0 +1,16 @@
+// Variables Declaration
+
+num1 = 5;
+
+num2 = 6;
+
+//
+
+
+// Arrays Declaration
+
+const europeanCapitals = ["Rome", "Paris", "Madrid", "Berlin", "London", "Moscow", "Vienna"];
+
+const valuesInRange = [];
+
+//
